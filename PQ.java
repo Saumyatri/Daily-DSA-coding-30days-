ccc0065f161ffa1d134280006fe4b817b6b2e6e5
Saumyatri <tripathisaumya120@gmail.com>
2024-06-09 12:18:36 +0530

    OOPs cocept

---
+++ PQ.java
@@ -0,0 +1,13 @@
+public class PQ {
+    public static void main(String args[]){
+        Student s= new Student();
+        s.name = "Aman";
+        System.out.println(s.name);
+
+    }
+}
+
+class Student{
+    String name;
+    int marks;
+}
