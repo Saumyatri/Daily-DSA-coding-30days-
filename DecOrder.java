1c99b47f9ba46a8d8a5aa3915aed92b2df94109e
Saumyatri <tripathisaumya120@gmail.com>
2024-06-12 10:25:54 +0530

    OOPs cocept

--- DecOrder.java
+++ DecOrder.java
@@ -38,8 +38,18 @@ public class DecOrder {
         int sn=n+snm1;
         return sn;
     }
+    
+    //FIBONACCI NUMBERS
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return n;}
+        int fibnm2=fib(n-2);
+        int fibnm1=fib(n-1);
+        int fbn=fibnm1+fibnm2;
+        return fbn;
+    }
     public static void main(String[] args) {
         int n=5;
-        System.out.println(calcSum(n));
+        System.out.println(fib(n));
     }
 }
