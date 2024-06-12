9d65925b8d14d862a0caa50003ddec47ba7edb83
Saumyatri <tripathisaumya120@gmail.com>
2024-06-12 08:31:55 +0530

    OOPs cocept

--- DecOrder.java
+++ DecOrder.java
@@ -28,8 +28,18 @@ public class DecOrder {
         int fn=n*fact(n-1);
         return fn;
     }
+
+    //sum of n natural numbers
+    public static int calcSum(int n){
+        if(n==1){
+            return 1;
+        }
+        int snm1=calcSum(n-1);
+        int sn=n+snm1;
+        return sn;
+    }
     public static void main(String[] args) {
         int n=5;
-        System.out.println(fact(n));
+        System.out.println(calcSum(n));
     }
 }
