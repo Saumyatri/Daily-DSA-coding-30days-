b7f32f201bf6db412568c4588eb4d0182a8d871b
Saumyatri <tripathisaumya120@gmail.com>
2024-06-27 07:28:52 +0530

    Recursion Basic without optimization

--- DecOrder.java
+++ DecOrder.java
@@ -199,8 +199,23 @@ public class DecOrder {
                 return 0;
             }
             return stringLength(str.substring(1))+1;
+        }
 
-
+        //Contiguous Substring
+        public static int countSubstrs(String str,  int i, int j, int n){
+            if (n == 1) {
+                return 1;
+                }
+                if (n <= 0) {
+                return 0;
+                }
+                int res = countSubstrs(str, i + 1, j, n - 1) +
+                countSubstrs(str, i, j - 1, n - 1) -
+                countSubstrs(str, i + 1, j - 1, n - 2);
+                if (str.charAt(i) == str.charAt(j)) {
+                    res++;
+                    }
+            return res;
         }
     public static void main(String[] args) {
        // int n=2;
@@ -210,9 +225,12 @@ public class DecOrder {
         //int key=2;
         //allOccurence(arr, key, 0);
         //printDigits(1290);
-        String str="abcdefg";
-        System.out.println(stringLength(str));
+        //String str="abcdefg";
+        //System.out.println(stringLength(str));
         //removeDuplicates(0, str,new StringBuilder(""), new boolean[26]);
+        String str = "abcab";
+        int n = str.length();
+        System.out.print(countSubstrs(str, 0, n-1, n));
     
     }
 }
